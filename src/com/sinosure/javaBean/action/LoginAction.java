bb1e25c6112190ce6dadcc961ba54a674f6ff4ae
Administrator <Administrator@A3ICC4DJ68KLP61>
2017-12-13 21:08:34 +0800

    提交到new

--- src/com/sinosure/javaBean/action/LoginAction.java
+++ src/com/sinosure/javaBean/action/LoginAction.java
@@ -3,11 +3,6 @@ package com.sinosure.javaBean.action;
 public class LoginAction {
 
 	public void testGit() {
-		System.out.println("测试提交git代码到远端服务器");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
+		System.out.println("测试提交git代码到new");
 	}
 }
