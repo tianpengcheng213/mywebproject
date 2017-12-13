3ab73a0d6b1e01c0e9565a8814e18cf52b7b6087
Administrator <Administrator@A3ICC4DJ68KLP61>
2017-12-13 21:17:50 +0800

    修改完成提交到开发环境

--- src/com/sinosure/javaBean/action/LoginAction.java
+++ src/com/sinosure/javaBean/action/LoginAction.java
@@ -4,21 +4,6 @@ public class LoginAction {
 
 	public void testGit() {
 		System.out.println("测试提交git代码到远端服务器");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		System.out.println("测试新分支提交");
-		
+		System.out.println();
 	}
 }
